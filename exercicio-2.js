7d4f3b3cf7c0aa4ed315d331610a564e69cc7962
Joely Brito <joely_brito@outlook.com>
2022-03-06 19:48:55 -0300

    refatoracao e adicao de exemplos

--- exercicio-2.js
+++ exercicio-2.js
@@ -1,4 +1,4 @@
-const numeros = [1, 5, 3, 4, 2];
+
 function ordenaValoresDecrescentes(a, b) {
     if(a > b) {
         return -1;
@@ -9,13 +9,25 @@ function ordenaValoresDecrescentes(a, b) {
     return 0;
 }
 
-let paresFormados = 0;
-numeros.forEach(function percorre(a) {
-    numeros.forEach(function comparacao(b) {
-        if(a - b == 2) {
-            paresFormados++;
-        }
+function mostraParesFormados(numeros, x) {
+    let paresFormados = 0;
+    numeros.forEach(function percorre(a) {
+        numeros.forEach(function comparacao(b) {
+            if(a - b == x) {
+                paresFormados++;
+            }
+        })
     })
-})
+    
+    console.log('Quantidade de pares formados é: ' + paresFormados);
+}
 
-console.log('Quantidade de pares formados é: ' + paresFormados);
+const exemplo1 = [1, 5, 3, 4, 2];
+const exemplo2 = [4, 8, 2, 3, 7, 5];
+const intervalo1 = 2;
+const intervalo2 = 1;
+
+mostraParesFormados(exemplo1, intervalo1);
+mostraParesFormados(exemplo2, intervalo2);
+
+
